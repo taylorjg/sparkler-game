9cbd66434537a10027613a178056f595d8068b9b
Administrator <jonathan.taylor.programmer@gmail.com>
2020-01-04 10:19:38 +0000

    A bit of refactoring re globals

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 /* eslint-env browser */
 
-let canvas = document.getElementById('canvas')
-let width = canvas.scrollWidth
-let height = canvas.scrollHeight
-let ctx = canvas.getContext('2d')
-
-canvas.width = width
-canvas.height = height
-
-const randomVelocity = () => (Math.random() - 0.5) * 2.5
-
 const COLOURS = [
   '#ffffff',
   '#ffffff',
@@ -20,97 +10,130 @@ const COLOURS = [
   '#90ee90',
   '#90ee90'
 ]
-
-const getTimestamp = () => new Date().getTime()
-
 const GRAVITY = 9.81
-const BOOST = GRAVITY * 2
+const BOOST = GRAVITY * 2.5
 const PARTICLE_ITERATIONS = 50
 const PARTICLE_SIZE = 5
 const MARGIN_Y = 50
-const MIN_Y = MARGIN_Y
-const MAX_Y = height - MARGIN_Y
-const INITIAL_X = width * 0.25
-const INITIAL_Y = MAX_Y
-let numBoosts = 0
-let currentVelocityY = 0
-let currentY = INITIAL_Y
-let particles = []
-let lastRenderTimestamp = getTimestamp()
+const MAX_BOOSTS = 8
+const UP_ARROW_KEY = 38
+
+const globals = {
+  CANVAS: undefined,
+  CTX: undefined,
+  WIDTH: 0,
+  HEIGHT: 0,
+  MIN_Y: 0,
+  MAX_Y: 0,
+  INITIAL_X: 0,
+  INITIAL_Y: 0,
+
+  currentVelocityY: 0,
+  currentY: 0,
+  remainingBoosts: 0,
+  particles: [],
+  lastRenderTimestamp: 0
+}
+
+const getTimestamp = () => new Date().getTime()
+
+const randomVelocity = () => (Math.random() - 0.5) * 2.5
 
 const createParticle = () => ({
-  x: INITIAL_X,
-  y: currentY,
+  x: globals.INITIAL_X,
+  y: globals.currentY,
   velocityX: randomVelocity(),
   velocityY: randomVelocity(),
   iterations: PARTICLE_ITERATIONS,
   size: PARTICLE_SIZE
 })
 
-const calculateMovementY = deltaTime => {
-  if (currentY <= MIN_Y) {
-    currentY = MIN_Y
+const calculateAccelerationY = deltaTime => {
+
+  const MULTIPLIER = 100
+
+  if (globals.currentY <= globals.MIN_Y) {
     const accelerationY = GRAVITY
-    currentVelocityY = accelerationY * deltaTime
-    const movementY = currentVelocityY * deltaTime * 100
+    globals.currentVelocityY = accelerationY * deltaTime
+    const movementY = globals.currentVelocityY * deltaTime * MULTIPLIER
     return movementY
   }
 
-  if (currentY >= MAX_Y) {
-    currentY = MAX_Y
-    const accelerationY = 0 - (numBoosts ? BOOST : 0)
-    currentVelocityY = accelerationY * deltaTime
-    const movementY = currentVelocityY * deltaTime * 100
+  if (globals.currentY >= globals.MAX_Y) {
+    const accelerationY = 0 - (globals.remainingBoosts ? BOOST : 0)
+    globals.currentVelocityY = accelerationY * deltaTime
+    const movementY = globals.currentVelocityY * deltaTime * MULTIPLIER
     return movementY
   }
 
-  const accelerationY = GRAVITY - (numBoosts ? BOOST : 0)
-  currentVelocityY += accelerationY * deltaTime
-  const movementY = currentVelocityY * deltaTime * 100
+  const accelerationY = GRAVITY - (globals.remainingBoosts ? BOOST : 0)
+  globals.currentVelocityY += accelerationY * deltaTime
+  const movementY = globals.currentVelocityY * deltaTime * MULTIPLIER
   return movementY
 }
 
-const stillActive = p => p.iterations > 0
-
 const render = () => {
-  particles.push(createParticle())
+  globals.particles.push(createParticle())
   const thisRenderTimestamp = getTimestamp()
-  const deltaTime = (thisRenderTimestamp - lastRenderTimestamp) / 1000
-  lastRenderTimestamp = thisRenderTimestamp
-  const movementY = calculateMovementY(deltaTime)
-  currentY += movementY
-  if (numBoosts > 0) {
-    numBoosts -= 1
+  const deltaTime = (thisRenderTimestamp - globals.lastRenderTimestamp) / 1000
+  globals.lastRenderTimestamp = thisRenderTimestamp
+  const movementY = calculateAccelerationY(deltaTime)
+  globals.currentY += movementY
+  if (globals.remainingBoosts > 0) {
+    globals.remainingBoosts -= 1
   }
-  ctx.clearRect(0, 0, width, height)
-  particles.forEach(particle => {
+  globals.CTX.clearRect(0, 0, globals.WIDTH, globals.HEIGHT)
+  globals.particles.forEach(particle => {
     const { x, y, velocityX, velocityY, iterations, size } = particle
     const numColours = COLOURS.length
     const colourIndex = numColours - Math.floor(iterations / PARTICLE_ITERATIONS * numColours)
     const rgbStr = COLOURS[colourIndex]
     const alpha = Math.floor(iterations / PARTICLE_ITERATIONS * 255)
     const alphaStr = alpha.toString(16).padStart(2, '0')
-    ctx.fillStyle = rgbStr + alphaStr
-    ctx.fillRect(x, y, size, size)
+    globals.CTX.fillStyle = rgbStr + alphaStr
+    globals.CTX.fillRect(x, y, size, size)
     particle.x += velocityX
     particle.y += velocityY + movementY
     particle.iterations -= 1
     particle.size *= 0.99
   })
-  particles = particles.filter(stillActive)
-  if (particles.length === 0) {
-    currentVelocityY = 0
-    currentY = INITIAL_Y
+  globals.particles = globals.particles.filter(p => p.iterations > 0)
+  if (globals.particles.length === 0) {
+    globals.currentVelocityY = 0
+    globals.currentY = globals.INITIAL_Y
   }
   requestAnimationFrame(render)
 }
 
 const onKeyDown = e => {
-  if (e.keyCode === 38) {
-    numBoosts = 12
+  if (e.keyCode === UP_ARROW_KEY) {
+    if (globals.remainingBoosts === 0) {
+      globals.remainingBoosts = MAX_BOOSTS
+    }
   }
 }
 
-document.addEventListener('keydown', onKeyDown)
+const init = () => {
+  const canvas = document.getElementById('canvas')
+  const width = canvas.scrollWidth
+  const height = canvas.scrollHeight
+  canvas.width = width
+  canvas.height = height
 
-render()
+  globals.CTX = canvas.getContext('2d')
+  globals.WIDTH = width
+  globals.HEIGHT = height
+  globals.MIN_Y = MARGIN_Y
+  globals.MAX_Y = height - MARGIN_Y
+  globals.INITIAL_X = width * 0.25
+  globals.INITIAL_Y = globals.MAX_Y
+
+  globals.currentY = globals.INITIAL_Y
+  globals.lastRenderTimestamp = getTimestamp()
+
+  document.addEventListener('keydown', onKeyDown)
+
+  render()
+}
+
+init()
