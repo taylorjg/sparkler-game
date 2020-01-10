b3f5a85c5afce43388f6260c044320578cc582e7
Administrator <jonathan.taylor.programmer@gmail.com>
2020-01-10 00:13:53 +0000

    Cache files for offline use in service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,26 @@
 console.log('[service-worker]')
 
+const CACHE_NAME = 'cache-v1'
+
+const urlsToCache = [
+  '/index.html',
+  '/styles.css',
+  '/VectorBattle-e9XO.ttf',
+  '/bundle.js',
+  '/stream-processor.js'
+]
+
 self.addEventListener('install', event => {
   console.log('[service-worker install handler]')
   console.dir(event)
+  self.skipWaiting()
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[service-worker install handler] cache open')
+        return cache.addAll(urlsToCache)
+      })
+  )
 })
 
 self.addEventListener('activate', event => {
